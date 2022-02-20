e3e777b34cfe82ca87389ad2d8795e40c7c9c617
Anton Balter <antbalter@gmail.com>
2022-02-21 00:55:18 +0200

    HW#21

--- chessBoard.js
+++ chessBoard.js
@@ -1,70 +1,48 @@
 class ChessBoard {
     constructor(parentElem, clickFn) {
         this.parentElem = document.getElementById(parentElem);
-        const board = this.parentElem;
-        const alert = this.clickFn;
+        this.clickFn = clickFn;
+
     }
-    render(board, alert){
+    render(clickFn){
         for (let i = 0; i < 10; i++) {
             const row = document.createElement("div");
-            row.classList.add("row");
-            row.classList.add("g-0");
+            row.classList.add("row", "g-0");
             row.id = i;
-            board.appendChild(row);
+            this.parentElem.appendChild(row);
             for (let j = 64; j < 74; j++) {
                 const  column = document.createElement("div");
-                column.classList.add("col");
-                column.classList.add("g-0");
+                column.classList.add("col", "g-0");
                 if(((i == 0) && (j == 64)) || ((i == 9) && (j == 64)) || ((i == 0) && (j == 73)) || ((i == 9) && (j == 73))){
                     row.appendChild(column);
-                } else if((i == 0) && (j > 64) && (j < 73)) {
-                    column.classList.add("d-flex");
-                    column.classList.add("align-items-end");
-                    column.classList.add("justify-content-center");
-                    const par = document.createElement("p");
-                    const text = document.createTextNode(`${String.fromCharCode(j)}`);
-                    par.appendChild(text);
-                    column.appendChild(par);
-                    row.appendChild(column);
-                } else if((i == 9) && (j > 64) && (j < 73)){
-                    column.classList.add("d-flex");
-                    column.classList.add("align-items-start");
-                    column.classList.add("justify-content-center");
-                    const par = document.createElement("p");
-                    const text = document.createTextNode(`${String.fromCharCode(j)}`);
-                    par.appendChild(text);
-                    column.appendChild(par);
-                    row.appendChild(column);
-                } else if((j == 73) && (i > 0) && (i < 9)) {
-                    column.classList.add("d-flex");
-                    column.classList.add("align-items-center");
-                    column.classList.add("justify-content-start");
-                    const par = document.createElement("p");
-                    const text = document.createTextNode(`${i}`);
-                    par.appendChild(text);
-                    column.appendChild(par);
-                    row.appendChild(column);
-                } else if((j == 64) && (i > 0) && (i < 9)) {
-                    column.classList.add("d-flex");
-                    column.classList.add("align-items-center");
-                    column.classList.add("justify-content-end");
-                    const par = document.createElement("p");
-                    const text = document.createTextNode(`${i}`);
-                    par.appendChild(text);
-                    column.appendChild(par);
-                    row.appendChild(column);
-                } else {                    
-                    column.classList.add("ratio");
-                    column.classList.add("ratio-1x1");
-                    column.classList.add("border");
+                } else if ((j > 64) && (j < 73) && (i > 0) && (i < 9)){                    
+                    column.classList.add("ratio", "ratio-1x1", "border");
                     column.id = `${String.fromCharCode(j)}${i}`;
                     row.appendChild(column);
                     ((i+j) % 2 != 0) && column.classList.add("bg-dark");
                     document.getElementById(`${String.fromCharCode(j)}${i}`).addEventListener("click", function(event) {
                         event.preventDefault();
-                        alert(i,j);
+                        clickFn(i,j);
                         });
-                }        
+                } else { 
+                    column.classList.add("d-flex");
+                    const par = document.createElement("p");
+                    let text = "";
+                    if((i == 0) && (j > 64) && (j < 73)) {
+                        column.classList.add("align-items-end", "justify-content-center");
+                        text = document.createTextNode(`${String.fromCharCode(j)}`);
+                    } else if((i == 9) && (j > 64) && (j < 73)) {
+                        column.classList.add("align-items-start", "justify-content-center");
+                        text = document.createTextNode(`${String.fromCharCode(j)}`);
+                    } else if((j == 73) && (i > 0) && (i < 9)) {
+                        column.classList.add("align-items-center", "justify-content-start");
+                        text = document.createTextNode(`${i}`);
+                    } else if((j == 64) && (i > 0) && (i < 9)) {
+                        column.classList.add("align-items-center", "justify-content-end");
+                        text = document.createTextNode(`${i}`);
+                    }
+                    row.appendChild(column).appendChild(par).appendChild(text);
+                };                       
             }            
         }
     }
--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-const root = document.getElementById('root');
-const chessBoard = new ChessBoard(root, alert);
-chessBoard.render(root, alert);
+const chessBoard = new ChessBoard('root', alert);
+chessBoard.render(alert);
 function alert(i, j){
     const note = document.createElement("div");
-    note.classList.add("alert");
-    note.classList.add("alert-warning");
-    note.classList.add("alert-dismissible");
-    note.classList.add("fade");
-    note.classList.add("show");
-    note.classList.add("g-0")
-    note.classList.add("p-0")
-    note.classList.add("m-0")
+    note.classList.add("alert", "alert-warning", "alert-dismissible", "fade", "show", "g-0","p-0", "m-0");
     note.setAttribute("role", "alert");
     const notePara = document.createElement("p");
     notePara.setAttribute("style", "font-size:2vw;")
     const noteText = document.createTextNode(`${String.fromCharCode(j)} ${i}`);
-    note.appendChild(notePara);
-    notePara.appendChild(noteText);
-    document.getElementById(`${String.fromCharCode(j)}${i}`).appendChild(note);
+    document.getElementById(`${String.fromCharCode(j)}${i}`).appendChild(note).appendChild(notePara).appendChild(noteText);
     note.id = `${String.fromCharCode(j)}${i}note`
     const closeBtn = document.createElement("button");
     closeBtn.setAttribute("type", "button");
-    closeBtn.classList.add("btn-close");
-    closeBtn.classList.add("g-0")
-    closeBtn.classList.add("p-0")
-    closeBtn.classList.add("m-0")
+    closeBtn.classList.add("btn-close", "g-0", "p-0", "m-0");
     closeBtn.setAttribute("aria-label", "Close");
     document.getElementById(`${String.fromCharCode(j)}${i}note`).appendChild(closeBtn);
     closeBtn.id = `${String.fromCharCode(j)}${i}close`;
@@ -35,8 +22,7 @@ function alert(i, j){
         });
     document.getElementById(`${String.fromCharCode(j)}${i}note`).addEventListener("click", function(event) {        
         event.preventDefault();
-        event.stopPropagation();
-        document.getElementById(`${String.fromCharCode(j)}${i}note`).remove();        
+        event.stopPropagation();      
         });        
         
 }
