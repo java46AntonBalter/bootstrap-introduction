ccbc75862b3dc95b15f69ac1b2fee56511f6dd39
Anton Balter <antbalter@gmail.com>
2022-02-21 19:23:12 +0200

    HW#21

--- chessBoard.js
+++ chessBoard.js
@@ -1,49 +1,58 @@
+const letters = ["A", "B", "C", "D", "E", "F", "G", "H" ];
+const matrix = getChessMatrix();
+let clickFn;
 class ChessBoard {
-    constructor(parentElem, clickFn) {
-        this.parentElem = document.getElementById(parentElem);
-        this.clickFn = clickFn;
-
+    constructor(idParent, clickFnP) {
+        this.parentElem = document.getElementById(idParent);
+        clickFn = clickFnP;
     }
-    render(clickFn){
-        for (let i = 0; i < 10; i++) {
-            const row = document.createElement("div");
-            row.classList.add("row", "g-0");
-            row.id = i;
-            this.parentElem.appendChild(row);
-            for (let j = 64; j < 74; j++) {
-                const  column = document.createElement("div");
-                column.classList.add("col", "g-0");
-                if(((i == 0) && (j == 64)) || ((i == 9) && (j == 64)) || ((i == 0) && (j == 73)) || ((i == 9) && (j == 73))){
-                    row.appendChild(column);
-                } else if ((j > 64) && (j < 73) && (i > 0) && (i < 9)){                    
-                    column.classList.add("ratio", "ratio-1x1", "border");
-                    column.id = `${String.fromCharCode(j)}${i}`;
-                    row.appendChild(column);
-                    ((i+j) % 2 != 0) && column.classList.add("bg-dark");
-                    document.getElementById(`${String.fromCharCode(j)}${i}`).addEventListener("click", function(event) {
-                        event.preventDefault();
-                        clickFn(i,j);
-                        });
-                } else { 
-                    column.classList.add("d-flex");
-                    const par = document.createElement("p");
-                    let text = "";
-                    if((i == 0) && (j > 64) && (j < 73)) {
-                        column.classList.add("align-items-end", "justify-content-center");
-                        text = document.createTextNode(`${String.fromCharCode(j)}`);
-                    } else if((i == 9) && (j > 64) && (j < 73)) {
-                        column.classList.add("align-items-start", "justify-content-center");
-                        text = document.createTextNode(`${String.fromCharCode(j)}`);
-                    } else if((j == 73) && (i > 0) && (i < 9)) {
-                        column.classList.add("align-items-center", "justify-content-start");
-                        text = document.createTextNode(`${i}`);
-                    } else if((j == 64) && (i > 0) && (i < 9)) {
-                        column.classList.add("align-items-center", "justify-content-end");
-                        text = document.createTextNode(`${i}`);
-                    }
-                    row.appendChild(column).appendChild(par).appendChild(text);
-                };                       
-            }            
-        }
+    render() {
+        this.parentElem.innerHTML = `${getTitle()}${getContent()}${getTitle()}`
     }
 }
+function getTitle() {
+    return `${getRow()}${getEmptyCell()}${getLetters()}${getEmptyCell()}</div>`
+}
+function getRow() {
+    return `<div class="row">`
+}
+function getEmptyCell() {
+    return `<div class="col-auto text-white">1</div>`
+}
+function getChessMatrix() {
+    const matrix = [];
+    let count = 0
+    for (let i = 0; i < 8; i++) {
+        matrix.push([]);
+        for (let j = 0; j < 8; j++) {
+            matrix[i][j] = count++ % 2 - i % 2;
+        }
+    }
+    return matrix;
+}
+function getContent() {
+    return matrix.map((r, i) => `${getChessRow(r,i)}</div>`).join('');
+}
+function getChessRow(row, index) {
+    return `${getRow()}${getRowNumber(index)}${getChessCells(row, index)}
+     ${getRowNumber(index)}`
+}
+function getChessCells(row, index) {
+    return row.map(getChessCell(index)).join('');
+}
+
+function getChessCell(index) {
+    return (cell, j) => `<div class="col-auto ${cell == 0 ? 'bg-dark' : 'bg-white'}
+     ${cell == 0 ? 'text-dark' : 'text-white'} border
+      " onclick="${clickFn.name}('${letters[j]}', ${index})" >${letters[j]}</div>`;
+}
+
+function getLetters() {
+    return letters.map(l => `<div class="col-auto border border-white" >${l}</div>`).join('')
+}
+function getRowNumber(num) {
+    return `<div class="col-auto">${num}</div>`
+}
+
+
+  
--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const chessBoard = new ChessBoard('root', alert);
-chessBoard.render(alert);
-function alert(i, j){
-    const note = document.createElement("div");
-    note.classList.add("alert", "alert-warning", "alert-dismissible", "fade", "show", "g-0","p-0", "m-0");
-    note.setAttribute("role", "alert");
-    const notePara = document.createElement("p");
-    notePara.setAttribute("style", "font-size:2vw;")
-    const noteText = document.createTextNode(`${String.fromCharCode(j)} ${i}`);
-    document.getElementById(`${String.fromCharCode(j)}${i}`).appendChild(note).appendChild(notePara).appendChild(noteText);
-    note.id = `${String.fromCharCode(j)}${i}note`
-    const closeBtn = document.createElement("button");
-    closeBtn.setAttribute("type", "button");
-    closeBtn.classList.add("btn-close", "g-0", "p-0", "m-0");
-    closeBtn.setAttribute("aria-label", "Close");
-    document.getElementById(`${String.fromCharCode(j)}${i}note`).appendChild(closeBtn);
-    closeBtn.id = `${String.fromCharCode(j)}${i}close`;
-    document.getElementById(`${String.fromCharCode(j)}${i}close`).addEventListener("click", function(event) {        
-        event.preventDefault();
-        event.stopPropagation();
-        document.getElementById(`${String.fromCharCode(j)}${i}note`).remove();        
-        });
-    document.getElementById(`${String.fromCharCode(j)}${i}note`).addEventListener("click", function(event) {        
-        event.preventDefault();
-        event.stopPropagation();      
-        });        
-        
+
+const CHESS_PARENT_ID = "chess-parent"
+const chessBoard = new ChessBoard(CHESS_PARENT_ID, chessCell);
+chessBoard.render();
+const alertParent = document.getElementById("alert");
+function chessCell(column, row) {
+    alertParent.innerHTML = `<div class="alert alert-warning alert-dismissible" role="alert">
+    <strong>You have pressed ${column + row}</strong> 
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>`
+
 }
